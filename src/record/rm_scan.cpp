398edacec8843189711470646142c1c2e5640a1d
Andy Lee <andylizf@outlook.com>
2025-01-04 16:56:56 +0800

    feat: record management

--- src/record/rm_scan.cpp
+++ src/record/rm_scan.cpp
@@ -9,6 +9,7 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 #include "rm_scan.h"
+
 #include "rm_file_handle.h"
 
 /**
@@ -16,32 +17,39 @@ See the Mulan PSL v2 for more details. */
  * @param file_handle
  */
 RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
-    // Todo:
-    // 初始化file_handle和rid（指向第一个存放了记录的位置）
-
+    rid_.page_no = 1;   // Start from first data page
+    rid_.slot_no = -1;  // Start before first slot
+    next();             // Move to first valid record
 }
 
 /**
  * @brief 找到文件中下一个存放了记录的位置
  */
 void RmScan::next() {
-    // Todo:
-    // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
+    while (rid_.page_no < file_handle_->file_hdr_.num_pages) {
+        RmPageHandle page_handle = file_handle_->fetch_page_handle(rid_.page_no);
 
+        rid_.slot_no =
+            Bitmap::next_bit(true, page_handle.bitmap, file_handle_->file_hdr_.num_records_per_page, rid_.slot_no);
+
+        if (rid_.slot_no != file_handle_->file_hdr_.num_records_per_page) {
+            return;
+        }
+
+        rid_.page_no++;
+        rid_.slot_no = -1;
+    }
+
+    rid_.page_no = RM_NO_PAGE;
+    rid_.slot_no = -1;
 }
 
 /**
  * @brief ​ 判断是否到达文件末尾
  */
-bool RmScan::is_end() const {
-    // Todo: 修改返回值
-
-    return false;
-}
+bool RmScan::is_end() const { return rid_.page_no == RM_NO_PAGE; }
 
 /**
  * @brief RmScan内部存放的rid
  */
-Rid RmScan::rid() const {
-    return rid_;
-}
+Rid RmScan::rid() const { return rid_; }
